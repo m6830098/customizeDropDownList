35ae3f61d01cd9ee5ddb3fa0bad70db6a4b89950
Chen Hao <chenhaoustc@gmail.com>
2014-05-05 11:04:55 +0800

    Initial Commit

---
+++ myCombox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myCombox
+//
+//  Created by ChenHao on 5/5/14.
+//  Copyright (c) 2014 ChenHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "myComboxAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([myComboxAppDelegate class]));
+    }
+}
---
+++ myCombox/myComboxAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  myComboxAppDelegate.h
+//  myCombox
+//
+//  Created by ChenHao on 5/5/14.
+//  Copyright (c) 2014 ChenHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myComboxAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
